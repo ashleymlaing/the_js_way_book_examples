fb8da38a6fac28169b7291717ea98efcc4e3d1cc
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-06-07 21:20:41 -0400

    working on finishing part 1

---
+++ chapter8/js/word_info.js
@@ -0,0 +1,6 @@
+let word = prompt("Write a word")
+let wordLength = word.length
+let lowercase = word.toLowerCase()
+let uppercase = word.toUpperCase()
+
+console.log(word, wordLength, lowercase, uppercase);
